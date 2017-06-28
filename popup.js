6887235203346e6a75e5922b34727188a35f9195
hzjis <huang_z_j@qq.com>
2017-06-28 11:24:58 +0800

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,9 @@
+﻿
+chrome.tabs.getSelected(null,function(tab){
+    var tit = tab.title;
+    var url = tab.url;
+    var imgSrc = 'https://www.kuaizhan.com/common/encode-png?large=true&data=' + url;
+    document.getElementById("tit").innerHTML = tit;
+    document.getElementById("code").src = imgSrc;
+});
+
